8cc05103aa5b10b3f879369f9431b82c6121574a
CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-06-28 23:55:18 +0300

    Conn string,appdbcontext program.cs finished for now  and ef framework is downloaded

---
+++ BotanikBambu.Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BotanikBambu.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+     
+
+        // DbSet'lerinizi buraya ekleyin
+        // public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
